09972e6db99c6adad3a42dada6160e21e7f00b8e
yth <yuting2727@gmail.com>
2023-09-08 20:43:14 +0000

    style

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ let service_dog = false;
 let phoneNumber;
 let latlng;
 
+const buttonStyle = {
+     padding: '2px 4px',  // Adjust the padding as needed
+     fontSize: '12px',  // Adjust the font size as needed
+                      };
+
 function StopReviewButton() {
   function Stop() {
     // Abort the fetch request by calling abort() on the AbortController instance
@@ -24,6 +29,7 @@ function StopReviewButton() {
     <button 
       id="stopBtn" 
       onClick={Stop}
+      style={buttonStyle}
       className="w-1/5 px-4 py-2 rounded-md border border-gray-500 text-gray-500 hover:text-gray-700 hover:border-gray-700 focus:outline-none ml-2 disabled:opacity-75 disabled:cursor-not-allowed"
     >
         Stop
@@ -117,6 +123,7 @@ function GenerateReviewButton() {
     <button 
       id="generateBtn" 
       onClick={Generate}
+      style={buttonStyle}
       className="w-4/5 px-4 py-2 rounded-md bg-black text-white hover:bg-gray-900 focus:outline-none mr-2 disabled:opacity-75 disabled:cursor-not-allowed"
     >
 	  Summarize Reviews
@@ -194,7 +201,7 @@ function PhoneNumberBox() {
   const handleChange = (event) => {
     phoneNumber = event.target.value; // Update phoneNumber directly
   }
-
+  
   return (
     <div>
       <input
@@ -202,6 +209,7 @@ function PhoneNumberBox() {
         placeholder="Phone Number"
         className="w-1/2 px-4 py-2 rounded-md bg-gray-200 placeholder-gray-500 focus:outline-none mt-4"
         onChange={handleChange}
+	style={buttonStyle}  // Apply the inline styles here
       />
     </div>
   );
@@ -247,6 +255,7 @@ function CallButton() {
     <button 
       id="callBtn" 
       onClick={Call}
+      style={buttonStyle}
       className="w-1/5 px-4 py-2 rounded-md bg-black text-white hover:bg-gray-900 focus:outline-none mr-2 disabled:opacity-75 disabled:cursor-not-allowed"
     >
         Call
